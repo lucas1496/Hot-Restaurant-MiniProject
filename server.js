f3b843fa38e6c9d14a165f795c6de41215c13b8e
Lucas Q <lucas.14.96@gmail.com>
2021-04-20 14:30:50 -0500

    Additional Updates

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -13,8 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-var customers = [];
-var waitlist = [];
+let customers = [];
+let waitlist = [];
 
 app.get('/', function(req,res) {
 	res.sendFile(path.join(__dirname, 'index.html'));
@@ -45,7 +45,7 @@ app.post('/api/clear', function(req, res) {
 
 app.post('/api/new', function(req, res) {
 	console.log('Works');
-	var newCustomer = req.body;
+	let newCustomer = req.body;
 	if (customers.length >= 2) {
 		waitlist.push(newCustomer);
 	} else {
